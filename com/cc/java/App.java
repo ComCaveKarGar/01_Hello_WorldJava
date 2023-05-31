f6308de5d65e04c748745326be0e4fccb0cd0ea0
Karsten <Moonjumper@gmx.de>
2023-05-31 16:31:15 +0200

    +1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,5 +5,15 @@ public class App {
     public static void main(String[] args) {
        System.out.println("Hello, World!");
     }
-    
+    // public <-- öffentlich static <-- statisch (nur in der Class) void <-- kein Dateityp (kein Rückgabewert) main <-- Name der Funktion 
+    // (String[]<-- Syntax args<-- arguments) {
+    //     System.out.println("Hello, World!");
+    //  }
+    public void testAusgabe(String[] args) {
+    System.out.println("Text zum Testen");
+ }
+// wird nicht ausgegeben da der wrapper fehlt! Siehe Klassen_Objekte_01_02
+
+
+
 }
